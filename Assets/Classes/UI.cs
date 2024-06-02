257d7eb18531e233598475299a7664af5a578592
MherMnatsakanyan03 <mher.mnatsakanyan03@gmail.com>
2024-06-02 11:05:55 +0200

    Added some UI functionalities

--- Assets/Classes/UI.cs
+++ Assets/Classes/UI.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
 using CSVReader = CityData.CSVReader;
@@ -7,7 +8,6 @@ using Year = CityData.Year;
 
 public class UI : MonoBehaviour
 {
-    // Start is called before the first frame update
     private VisualElement root;
     private Button nextYearButton,
         prevYearButton,
@@ -17,46 +17,166 @@ public class UI : MonoBehaviour
         greenBtn,
         blueBtn;
 
+    private IntegerField yearDisplay;
+
     private CSVReader csvReader;
     private List<Year> years;
 
     private int currentYearIndex = 0;
+    private int currentCityIndex = 0;
     private const int YEAR_OFFSET = 2005;
+
+    // Placeholder Cubes, shoukd be replaced with actual city data
+    private GameObject[] cubes;
 
     void OnEnable()
     {
-        csvReader = new CSVReader();
-        years = csvReader.GetYears();
-        root = GetComponent<UIDocument>().rootVisualElement;
+        LoadData();
+        GetUIComponents();
 
-        nextYearButton = root.Q<Button>("nextYear");
-        prevYearButton = root.Q<Button>("prevYear");
-        nextCityButton = root.Q<Button>("nextCity");
-        prevCityButton = root.Q<Button>("prevCity");
-        greenBtn = root.Q<Button>("btn1");
-        yellowBtn = root.Q<Button>("btn2");
-        blueBtn = root.Q<Button>("btn3");
+        InitButtonFunctions();
 
-        nextYearButton.clicked += NextYear;
-        prevYearButton.clicked += PrevYear;
-        nextCityButton.clicked += () => Debug.Log("Next City");
-        prevCityButton.clicked += () => Debug.Log("Previous City");
-        greenBtn.clicked += () => Debug.Log("Green Button");
-        yellowBtn.clicked += () => Debug.Log("Yellow Button");
-        blueBtn.clicked += () => Debug.Log("Blue Button");
+        // Initialize
+        SetYearDisplay(currentYearIndex);
+        cubes = GameObject.FindGameObjectsWithTag("Cube");
+        System.Array.Sort(
+            cubes,
+            (a, b) => a.transform.position.x.CompareTo(b.transform.position.x)
+        );
+        FocusCameraOnCity(currentCityIndex);
     }
 
-    private void NextYear() {
-        if (currentYearIndex < years.Count - 1) {
+    /* ================================================ Private Functions =============================================== */
+    /**
+     * Move to the next year
+     */
+    private void NextYear()
+    {
+        if (currentYearIndex < years.Count - 1)
+        {
             currentYearIndex++;
             years[currentYearIndex].printYearData();
         }
     }
 
-    private void PrevYear() {
-        if (currentYearIndex > 0) {
+    /**
+     * Move to the previous year
+     */
+    private void PrevYear()
+    {
+        if (currentYearIndex > 0)
+        {
             currentYearIndex--;
             years[currentYearIndex].printYearData();
+        }
+    }
+
+    /**
+     * Set the year display to the given count. Is offset by ${YEAR_OFFSET}
+     * @param count The count to set the year display to
+     */
+    private void SetYearDisplay(int count)
+    {
+        yearDisplay.value = count + YEAR_OFFSET;
+    }
+
+    /**
+     * Move to the next city. This is done by incrementing the currentCityIndex and setting the camera position to the new city
+     */
+    private void NextCity()
+    {
+        if (currentCityIndex < cubes.Length - 1)
+        {
+            currentCityIndex++;
+            FocusCameraOnCity(currentCityIndex);
+        }
+    }
+
+    /**
+     * Move to the previous city. This is done by decrementing the currentCityIndex and setting the camera position to the new city
+     */
+    private void PrevCity()
+    {
+        if (currentCityIndex > 0)
+        {
+            currentCityIndex--;
+            FocusCameraOnCity(currentCityIndex);
+        }
+    }
+
+    /**
+     * Focus the camera on the city at the given index
+     * @param cityIndex The index of the city to focus on
+     */
+    private void FocusCameraOnCity(int cityIndex)
+    {
+        Camera.main.transform.position = new Vector3(
+            cubes[cityIndex].transform.position.x,
+            cubes[cityIndex].transform.position.y,
+            -10
+        );
+    }
+
+    /**
+     * Load Data from CSV File
+     */
+    private void LoadData()
+    {
+        // Load Data
+        csvReader = new CSVReader();
+        years = csvReader.GetYears();
+    }
+
+    /**
+     * Get UI Components (Buttons, IntegerFields, etc.)
+     */
+    private void GetUIComponents()
+    {
+        // Get UI Elements
+        root = GetComponent<UIDocument>().rootVisualElement;
+        nextYearButton = root.Q<Button>("nextYear");
+        prevYearButton = root.Q<Button>("prevYear");
+        nextCityButton = root.Q<Button>("nextCity");
+        prevCityButton = root.Q<Button>("prevCity");
+        yearDisplay = root.Q<IntegerField>("yearDisplay");
+        greenBtn = root.Q<Button>("btn1");
+        yellowBtn = root.Q<Button>("btn2");
+        blueBtn = root.Q<Button>("btn3");
+    }
+
+    /**
+     * Initialize Button Click Events
+     */
+    private void InitButtonFunctions()
+    {
+        // Set Button Click Events
+        nextYearButton.clicked += () =>
+        {
+            NextYear();
+            SetYearDisplay(currentYearIndex);
+        };
+        prevYearButton.clicked += () =>
+        {
+            PrevYear();
+            SetYearDisplay(currentYearIndex);
+        };
+        nextCityButton.clicked += NextCity;
+        prevCityButton.clicked += PrevCity;
+        greenBtn.clicked += () => SetCubeTextColor(Color.green);
+        yellowBtn.clicked += () => SetCubeTextColor(Color.yellow);
+        blueBtn.clicked += () => SetCubeTextColor(Color.blue);
+    }
+
+    /**
+     * Set the color of the text in the cube-object
+     * @param color The color-object to set the text to
+     */
+    private void SetCubeTextColor(Color color)
+    {
+        foreach (var cube in cubes)
+        {
+            // Set Text Mesh Pro color
+            cube.GetComponent<TMPro.TextMeshPro>().color = color;
         }
     }
 }
