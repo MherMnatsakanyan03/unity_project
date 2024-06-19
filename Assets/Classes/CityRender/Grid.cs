5a0270b56e373cb929ba15c68e75bda8b323bc07
MherMnatsakanyan03 <mher.mnatsakanyan03@gmail.com>
2024-06-19 13:11:41 +0200

    Removed Editor inclusion

--- Assets/Classes/CityRender/Grid.cs
+++ Assets/Classes/CityRender/Grid.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using CityData;
 using RectpackSharp;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.UIElements;
 
